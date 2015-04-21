8d487c4d6ef0ab972c7dd2928b2f2881a1103d88
DongMin Kim <kim135797531@naver.com>
2015-04-21 23:30:08 +0900

    change display debug information function to inline function in autotest-util.h

--- src/autotest/autotest-util.h
+++ src/autotest/autotest-util.h
@@ -1,17 +1,17 @@
-﻿#ifndef KDM_AUTOTEST_AUTOTEST_UTIL_H_
-#define KDM_AUTOTEST_AUTOTEST_UTIL_H_
+﻿#ifndef KDM_SRC_AUTOTEST_AUTOTEST_UTIL_H_
+#define KDM_SRC_AUTOTEST_AUTOTEST_UTIL_H_
 
 #include <iostream>
 
 #define TEST_FUNCTION_NAME __FUNCTION__
 #define TEST_FILE_NAME __FILE__
 
-void displayError(const char* p_rgcInfo){
+static void inline displayError(const char* p_rgcInfo){
 	std::cout << "Test failed in " << p_rgcInfo << std::endl;
-}
+};
 
-void displaySuccess(const char* p_rgcInfo){
+static void inline displaySuccess(const char* p_rgcInfo){
 	std::cout << "Test passed in " << p_rgcInfo << std::endl;
-}
+};
 
-#endif KDM_AUTOTEST_AUTOTEST_UTIL_H_
+#endif KDM_SRC_AUTOTEST_AUTOTEST_UTIL_H_
