95b719fdb0f637f1be835dff9b8c2177a165504d
DongMin Kim <kim135797531@naver.com>
2015-04-23 20:43:07 +0900

    declare CKDMFileWrite class

--- src/library/file-read-write.cpp
+++ src/library/file-read-write.cpp
@@ -1,14 +1,14 @@
 ﻿#include <iostream>
 #include "file-read-write.h"
 
-void CKDMFileRead::open(char* p_szFileName){
+void CKDMFileRead::open(const char* p_szFileName){
 	close();
 
 	this->m_file.m_szFileName = new char[strlen(p_szFileName) + 1];
 	memcpy(this->m_file.m_szFileName, p_szFileName, strlen(p_szFileName) + 1);
 
 	//바이너리로 읽음, std::ifstream::in은 내부 스트림에서 읽기
-	this->m_inputStream = std::ifstream(p_szFileName, std::ifstream::binary);
+	this->m_inputStream = std::ifstream(p_szFileName, std::ifstream::binary | std::ifstream::in);
 	//맨 끝으로 이동
 	this->m_inputStream.seekg(0, std::ifstream::end);
 	//위치 = 파일 크기
@@ -23,47 +23,47 @@ void CKDMFileRead::close(){
 		this->m_inputStream.close();
 	if (m_file.m_szFileName != 0)
 		delete this->m_file.m_szFileName;
-	if (m_file.m_tRawData != 0)
-		delete this->m_file.m_tRawData;
+	if (m_file.m_szRawData != 0)
+		delete this->m_file.m_szRawData;
 
 	this->m_file.m_nFileSize = -1;
 }
 
-char* CKDMFileRead::getData(char* p_tDest, int p_iStart, int p_iSize){
-	if (this->m_file.m_tRawData != 0)
-		memcpy(p_tDest, &(this->m_file.m_tRawData[p_iStart]), sizeof(char)*p_iSize);
+char* CKDMFileRead::getData(char* p_szDest, int p_iStart, int p_iSize){
+	if (this->m_file.m_szRawData != 0)
+		memcpy(p_szDest, &(this->m_file.m_szRawData[p_iStart]), sizeof(char)*p_iSize);
 	else
-		readRange(p_tDest, p_iStart, p_iSize);
+		readRange(p_szDest, p_iStart, p_iSize);
 
-	return p_tDest;
+	return p_szDest;
 }
 
 char* CKDMFileRead::readAll(){
 	if (isOpen() != true)
 		throw std::logic_error("Error: File Not Opened");
-	if (this->m_file.m_tRawData != 0)
-		return this->m_file.m_tRawData;
+	if (this->m_file.m_szRawData != 0)
+		return this->m_file.m_szRawData;
 
 	//파일 내용을 가져오기 위한 영역 확보
-	this->m_file.m_tRawData = new char[this->m_file.m_nFileSize];
+	this->m_file.m_szRawData = new char[this->m_file.m_nFileSize];
 
 	//파일에서 읽기
 	int iCurIndex = static_cast<int>(this->m_inputStream.tellg());
 	this->m_inputStream.seekg(0, std::ifstream::beg);
-	this->m_inputStream.read(this->m_file.m_tRawData, sizeof(char)*this->m_file.m_nFileSize);
+	this->m_inputStream.read(this->m_file.m_szRawData, sizeof(char)*this->m_file.m_nFileSize);
 	this->m_inputStream.seekg(0, iCurIndex);
 
-	return m_file.m_tRawData;
+	return m_file.m_szRawData;
 }
 
-char* CKDMFileRead::readRange(char* p_tDest, int p_iStart, int p_iSize){
+char* CKDMFileRead::readRange(char* p_szDest, int p_iStart, int p_iSize){
 	if (isOpen() != true)
 		throw std::logic_error("Error: File Not Opened");
 
 	int iCurIndex = static_cast<int>(this->m_inputStream.tellg());
 	this->m_inputStream.seekg(p_iStart, std::ifstream::beg);
-	this->m_inputStream.read(p_tDest, sizeof(char)*p_iSize);
+	this->m_inputStream.read(p_szDest, sizeof(char)*p_iSize);
 	this->m_inputStream.seekg(0, iCurIndex);
 
-	return p_tDest;
+	return p_szDest;
 }
--- src/library/file-read-write.h
+++ src/library/file-read-write.h
@@ -6,33 +6,33 @@
 typedef struct SKDMFile{
 	char* m_szFileName = 0;
 	int m_nFileSize = -1;
-	char* m_tRawData = 0;
+	char* m_szRawData = 0;
 }SKDMFile;
 
-
+//데이터 보호를 위해 읽기만 가능하다.
 class CKDMFileRead{
 public:
-	CKDMFileRead(char* p_szFileName){ open(p_szFileName); }
+	CKDMFileRead(const char* p_szFileName){ open(p_szFileName); }
 	~CKDMFileRead(){ close(); }
 
-	void open(char* p_szFileName);
+	void open(const char* p_szFileName);
 	void close();
 
-	inline bool isOpen(){ return this->m_inputStream.is_open(); }
+	inline bool isOpen() const { return this->m_inputStream.is_open(); }
 	inline std::ifstream* getStream(){ return &(this->m_inputStream); }
 	inline const char* getFileName() const { return this->m_file.m_szFileName; }
 	inline const int getFileSize() const { return this->m_file.m_nFileSize; }
 
 	//주어진 배열에 복사도 하고, 서비스로 그거 리턴도 해줌;
-	char* getData(char* p_tDest, int p_iStart, int p_iSize);
+	char* getData(char* p_szDest, int p_iStart, int p_iSize);
 	char* getRawData(){ return this->readAll(); }
 		
-	char* operator()(char* p_tDest, int p_iStart, int p_iSize){	return this->getData(p_tDest, p_iStart, p_iSize); }
+	char* operator()(char* p_szDest, int p_iStart, int p_iSize){ return this->getData(p_szDest, p_iStart, p_iSize); }
 	char* operator()(){ return this->getRawData(); }
 
 private:
 	char* readAll();
-	char* readRange(char* p_tDest, int p_iStart, int p_iSize);
+	char* readRange(char* p_szDest, int p_iStart, int p_iSize);
 
 	//스트림 복사 안되니까 이 클래스도 복사하면 안 됨!
 	CKDMFileRead(const CKDMFileRead&);
@@ -42,4 +42,44 @@ private:
 	SKDMFile m_file;
 };
 
+//쓰기만 가능하다.
+class CKDMFileWrite{
+public:
+	const enum WriteMode{
+		WRITE_TRUNCATE = 0,
+		WRITE_APPEND = 1
+	};
+public:
+	CKDMFileWrite(const char* p_szFileName, WriteMode p_eMode = WRITE_APPEND){ open(p_szFileName, p_eMode); }
+	~CKDMFileWrite(){ close(); }
+
+	void open(const char* p_szFileName, WriteMode p_eMode);
+	void close();
+
+	inline bool isOpen() const { return this->m_outputStream.is_open(); }
+	inline std::ofstream* getStream(){ return &(this->m_outputStream); }
+	inline const char* getFileName() const { return this->m_szFileName; }
+	
+	//현재 모드로 데이터 기록
+	bool writeData(const char* p_szSrc, int p_nFileSize);
+	bool writeData(const SKDMFile* p_KDMFile);
+	bool operator()(const char* p_szDest, int p_nFileSize){ return this->writeData(p_szDest, p_nFileSize); }
+	bool operator()(const SKDMFile* p_KDMFile){ return this->writeData(p_KDMFile->m_szRawData, p_KDMFile->m_nFileSize); }
+
+	bool changeMode(WriteMode p_eMode);
+
+	bool modifyData(const char* p_szSrc, int p_iStart, int p_iSize);
+
+private:
+	bool writeDataTruncate(const char* p_szSrc);
+	bool writeDataAppend(const char* p_szSrc);
+
+	//스트림 복사 안되니까 이 클래스도 복사하면 안 됨!
+	CKDMFileWrite(const CKDMFileWrite&);
+	CKDMFileWrite& operator=(const CKDMFileWrite&);
+
+	char* m_szFileName = 0;
+	std::ofstream m_outputStream;
+	WriteMode m_eCurrentMode;
+};
 #endif KDM_SRC_LIBRARY_FILE_READ_WRITE_H
